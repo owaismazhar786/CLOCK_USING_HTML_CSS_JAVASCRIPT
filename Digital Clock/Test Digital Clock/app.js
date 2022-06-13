4af3abf14d200ab131353569d348d1c8be2e19db
owaismazhar786 <100398204+owaismazhar786@users.noreply.github.com>
2022-06-13 16:12:32 +0500

    'ad'

---
+++ Digital Clock/Test Digital Clock/app.js
@@ -0,0 +1,19 @@
+function clock() {
+    const time = new Date();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+    document.getElementById('hour').innerHTML = hour;
+    document.getElementById('minute').innerHTML = ":" + minute;
+    document.getElementById('second').innerHTML = ":" + second;
+}
+setInterval(clock, 100);
---
+++ Temperature Converter/app.js
@@ -0,0 +1,9 @@
+var farenheit = 0;
+var celsius = 0;
+var kelvin = 0;
+
+celsius = (farenheit - 32) * 5/9;
+
+farenheit = (celsius * 9/5) + 32;
+
+kelvin = celsius + 273;
